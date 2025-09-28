180f76b018f655191fef9085e423adcd5c688868
noah-quetua <noah.quetua@stu.ocsb.ca>
2025-09-28 15:46:52 +0000

    added main.ts file for Hello, World!

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World") ;
+/**
+ * @author Noah Q
+ * version 1.0.0
+ * @date 2025-09-27
+ * @fileoverview This is the traditional "Hello,World!" program.
+ */
+
+// print out the greeting
+console.log("Hello, Noah Quetua!") ;
+
+console.log("\nDone")
